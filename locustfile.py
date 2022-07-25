816dfd77e7a3060cb7cde068729540268ae6ab4b
Alefe Oliveira <alefe.oliveira@sinch.com>
2022-07-25 17:30:29 -0300

    changing host for explanation

--- locustfile.py
+++ locustfile.py
@@ -5,23 +5,11 @@ class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
 
     @task
-    def getMovies(self):
-        self.client.get("/restapis/" + str(os.getenv('API_GATEWAY_ID')) + "/dev/_user_request_/movies")
+    def googleTest(self):
+        self.client.get("/")
 
-    @task
-    def getBestMoviesRated(self):
-        self.client.get("/restapis/" + str(os.getenv('API_GATEWAY_ID')) + "/dev/_user_request_/best-movies-rated")
-    
-    @task
-    def rateMovie(self):
-        self.client.post("/restapis/" + str(os.getenv('API_GATEWAY_ID')) + "/dev/_user_request_/rate-movie", json={
-        "id": "438149",
-        "overview": "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
-        "poster_path": "/tzFAboMUGJKoPQEtlxfxbbYsSWa.jpg",
-        "release_date": "2022-06-29",
-        "title": "Minions: The Rise of Gru",
-        "grade_evaluated" : 1
-})
+
+
 
 
 
